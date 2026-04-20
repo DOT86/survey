7593f5fbf0d106581b945f9d4fd13062bdc86f68
dot86 <ignat-viz@mail.ru>
2026-04-20 17:24:13 +0300

    added current question proccess

---
+++ survey/quizes/services.py
@@ -0,0 +1,60 @@
+from typing import Optional
+
+from users.models import User
+from quizes.models import QuizSession
+
+class QuizSessionService:
+    def __init__(self):
+        self.quiz_question_service = QuizQuestionService()
+        self.question_answer_service = QuestionAnswerService()
+
+    def get_by_user(self, user: User) -> Optional[QuizSession] | None:
+        return QuizSession.objects.filter(user=user).first()
+
+    def get_by_user_and_quiz(self, user: User, quiz: Quiz) -> Optional[QuizSession] | None:
+        return QuizSession.objects.filter(user=user, quiz=quiz).first()
+
+    def get_current_question(self,user: User, quiz: Quiz) -> int | None:
+        quiz_session = self.get_by_user_and_quiz(user=user, quiz=quiz)
+        if not quiz_session:
+            return None
+        question_ids = self.quiz_question_service.get_ids_by_quiz(quiz_session.quiz)
+
+        current_question_id = None
+
+        # check answered questions
+        for question_id in question_ids:
+            question_answer = self.question_answer_service.get_by_question_id_and_session(
+                question_id=question_id, session=quiz_session)
+            if question_answer:
+                continue
+            current_question_id = question_id
+            break
+        return current_question_id
+
+
+class QuizQuestionService:
+    def __init__(self):
+        self.model = QuizQuestion
+
+    def get_by_id(self, id: int) -> Optional[QuizQuestion] | None:
+        return self.model.objects.filter(id=id).first()
+
+    def get_ids_by_quiz(self, quiz: Quiz) -> Optional[list[int]] | None:
+        return self.model.objects.filter(quiz=quiz).order_by('order').values_list('id', flat=True)
+
+    def get_next_question(self, answered_questions: list[int]) -> Optional[QuizQuestion] | None:
+        return self.model.objects.exclude(
+            id__in=answered_questions
+        ).order_by('order').first()
+
+
+class QuestionAnswerService:
+    def __init__(self):
+        self.model = QuestionAnswer
+
+    def get_by_question_id_and_session(self, question_id: int, session: QuizSession) -> Optional[QuestionAnswer] | None:
+        return self.model.objects.filter(question_id=question_id, session=session).first()
+
+
+
--- survey/quizes/views.py
+++ survey/quizes/views.py
@@ -1,8 +1,40 @@
-from rest_framework import permissions, viewsets
+from rest_framework import permissions, viewsets, views, status
 from quizes.models import Quiz
 from quizes.serializers import QuizSerializer
+from quizes.services import QuizSessionService
+from rest_framework.response import Response
+
 
 class QuizViewSet(viewsets.ModelViewSet):
     queryset = Quiz.objects.all()
     serializer_class = QuizSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+class CurrentQuestionView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+        quiz_id = request.query_params.get('quiz_id')
+        quiz_session_service = QuizSessionService()
+        quiz_question_service = QuizQuestionService()
+        if not quiz_id:
+            return Response({'error': 'Quiz ID not provided'}, status=status.HTTP_400_BAD_REQUEST)
+
+        quiz = Quiz.objects.filter(id=quiz_id).first()
+        if not quiz:
+            return Response({'error': 'Quiz not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        quiz_session = QuizSessionService().get_(user=user, quiz=quiz)
+        if not quiz_session:
+            return Response({'Error': 'Quiz session not found'}, status=status.HTTP_404_NOT_FOUND)
+        question_id = quiz_session_service.get_current_question(user=user, quiz=quiz)
+        if not question_id:
+            return Response({'Error': 'No more questions'}, status=status.HTTP_404_NOT_FOUND)
+        question = quiz_question_service.get_by_id(question_id)
+        if not question:
+            return Response({'Error': 'Question not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = QuizQuestionSerializer(question)
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
